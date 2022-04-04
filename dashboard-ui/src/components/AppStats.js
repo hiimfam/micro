624ddccff5a7adce2e3f12efae4c35eaf19166a8
William <53450249+hiimfam@users.noreply.github.com>
2022-04-03 17:26:59 -0700

    Add files via upload

---
+++ dashboard-ui/src/components/AppStats.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react'
+import '../App.css';
+
+export default function AppStats() {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [stats, setStats] = useState({});
+    const [error, setError] = useState(null)
+
+	const getStats = () => {
+	
+        fetch(`http://william-micro.eastus2.cloudapp.azure.com:8100/stats`)
+            .then(res => res.json())
+            .then((result)=>{
+				console.log("Received Stats")
+                setStats(result);
+                setIsLoaded(true);
+            },(error) =>{
+                setError(error)
+                setIsLoaded(true);
+            })
+    }
+    useEffect(() => {
+		const interval = setInterval(() => getStats(), 2000); // Update every 2 seconds
+		return() => clearInterval(interval);
+    }, [getStats]);
+
+    if (error){
+        return (<div className={"error"}>Error found when fetching from API</div>)
+    } else if (isLoaded === false){
+        return(<div>Loading...</div>)
+    } else if (isLoaded === true){
+        return(
+            <div>
+                <h1>Latest Stats</h1>
+                <table className={"StatsTable"}>
+					<tbody>
+						<tr>
+							<th>Artists</th>
+							<th>Songs</th>
+						</tr>
+						<tr>
+							<td># mc: {stats['num_artist_readings']}</td>
+							<td># ge: {stats['num_song_readings']}</td>
+						</tr>
+						<tr>
+							<td colspan="2">Highest Albums Recorded: {stats['max_albums_recorded_reading']}</td>
+						</tr>
+						<tr>
+							<td colspan="2">Highest Song Length: {stats['max_song_length_reading']}</td>
+						</tr>
+						{/* <tr>
+							<td colspan="2">Max HR: {stats['max_bp_sys_reading']}</td>
+						</tr> */}
+					</tbody>
+                </table>
+                <h3>Last Updated: {stats['last_updated']}</h3>
+
+            </div>
+        )
+    }
+}
